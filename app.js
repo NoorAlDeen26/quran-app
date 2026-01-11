4590b1a541002b2e7633cbcc738cd7b4ed7b9946
NoorAlDeen26 <ajmoney24@protonmail.com>
2026-01-11 20:09:22 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const surahSelect = document.getElementById("surahSelect");
+document.getElementById("quranText").innerHTML += ayahHtml;
 document.getElementById("leftPage").innerHTML += ayahHtml;
 const audio = document.getElementById("audio");
 const playBtn = document.getElementById("playBtn");
