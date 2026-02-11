bb7da9238fc0865f1f233144c7e24d9c347b88e6
NoorAlDeen26 <ajmoney24@protonmail.com>
2026-02-11 20:52:16 +0000

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const quranText = document.getElementById("quranText");
 const audioPlayer = document.getElementById("audioPlayer");
 const playBtn = document.getElementById("playBtn");
 
+let ayahsData = [];
+let currentAyahIndex = 0;
+let isPlaying = false;
+
+// Load Surah List
 for (let i = 1; i <= 114; i++) {
 let option = document.createElement("option");
 option.value = i;
@@ -13,47 +18,13 @@ surahSelect.appendChild(option);
 
 async function loadSurah(number) {
 quranText.innerHTML = "Loading...";
+currentAyahIndex = 0;
 
 const response = await fetch(`https://api.alquran.cloud/v1/surah/${number}/quran-uthmani`);
 const data = await response.json();
-const ayahs = data.data.ayahs;
+
+ayahsData = data.data.ayahs;
 
 quranText.innerHTML = "";
 
-ayahs.forEach(ayah => {
-let span = document.createElement("span");
-span.innerHTML = `${ayah.text} <span class="ayah-number">${ayah.numberInSurah}</span> `;
-quranText.appendChild(span);
-});
-
-updateAudio(number);
-}
-
-function updateAudio(number) {
-let formatted = number.toString().padStart(3, "0");
-let reciter = reciterSelect.value;
-
-let servers = {
-"abdul_basit_murattal": `https://server8.mp3quran.net/abdul_basit_murattal/${formatted}.mp3`,
-"afs": `https://server8.mp3quran.net/afs/${formatted}.mp3`,
-"sudais": `https://server8.mp3quran.net/sudais/${formatted}.mp3`,
-"shuraym": `https://server8.mp3quran.net/shuraym/${formatted}.mp3`,
-"minshawi": `https://server8.mp3quran.net/minshawi/${formatted}.mp3`
-};
-
-audioPlayer.src = servers[reciter];
-}
-
-playBtn.addEventListener("click", () => {
-audioPlayer.play();
-});
-
-surahSelect.addEventListener("change", () => {
-loadSurah(surahSelect.value);
-});
-
-reciterSelect.addEventListener("change", () => {
-updateAudio(surahSelect.value);
-});
-
-loadSurah(1);
+ayahsData.for
